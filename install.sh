5d5a9294f095f4fa7d26d4a56334dec096bb41a1
Timon Back <T.Back@student.rug.nl>
2017-10-15 18:31:39 +0200

    Added supervisior software

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Ethereum
 sudo apt-get install software-properties-common
 sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo apt-get update
 sudo apt-get install ethereum
 
+# ./scripts/INIT_CHAIN.sh
+# ./scripts/MINE_CHAIN.sh
+
+# Supervisor
+sudo apt-get install supervisor
+sudo cp ethereum-miner-worker.conf /etc/supervisor/conf.d/
+
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl stop all
+sudo supervisorctl start all
 
